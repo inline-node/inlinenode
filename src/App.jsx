0949705a6c9c0c4c736cbcea06fc2d5b1a38adbb
inline-node <admin@inlinenode.com>
2025-11-05 22:05:19 +0530

    Initial commit - Project setup Github Pages workflow

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600">InlineNode Works!</h1>
+      <p className="text-gray-700 dark:text-gray-300 mt-4">
+        Tailwind CSS is alive and well 🎉
+      </p>
+    </div>
+  );
+}
+
+export default App;
