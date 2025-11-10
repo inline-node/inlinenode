ae39340712ceac8f8ad4873c35b07b1d6b50ce78
inline-node <admin@inlinenode.com>
2025-11-10 12:11:30 +0530

    Update CurveLab.jsx
    layout update.

--- src/pages/CurveLab.jsx
+++ src/pages/CurveLab.jsx
@@ -1,27 +1,29 @@
 import Layout from "../layouts/Layout";
 import DataInput from "./curvelab/DataInput";
-import ModelSelector from "./curvelab/ModelSelector";
 import GraphArea from "./curvelab/GraphArea";
-import ConsoleArea from "./curvelab/ConsoleArea";
 import OutputSummary from "./curvelab/OutputSummary";
+import ConsoleArea from "./curvelab/ConsoleArea";
 
 export default function CurveLab() {
   return (
     <Layout>
-      <div className="max-w-6xl mx-auto p-6 flex flex-col gap-8">
-        <h1 className="text-3xl font-semibold text-accent dark:text-darkAccent">
-          CurveLab
-        </h1>
-        <p className="text-textDim dark:text-darkTextDim">
-          Analyze and fit data using various regression models. Import data, select models, visualize, and
-          view step-by-step derivations.
-        </p>
+      {/* Full height grid */}
+      <div className="h-[calc(100vh-80px)] w-full grid grid-rows-[2fr_1fr] grid-cols-[1.2fr_2fr] gap-3 p-3">
+        {/* Top Row */}
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-auto">
+          <DataInput />
+        </div>
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface">
+          <GraphArea />
+        </div>
 
-        <DataInput />
-        <ModelSelector />
-        <GraphArea />
-        <OutputSummary />
-        <ConsoleArea />
+        {/* Bottom Row */}
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-auto">
+          <OutputSummary />
+        </div>
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-auto">
+          <ConsoleArea />
+        </div>
       </div>
     </Layout>
   );
