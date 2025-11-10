0e52dd44f5585e783a827580759b1692dd172854
inline-node <admin@inlinenode.com>
2025-11-10 12:19:44 +0530

    Update CurveLab.jsx
    fixed scrolling issue.

--- src/pages/CurveLab.jsx
+++ src/pages/CurveLab.jsx
@@ -7,21 +7,24 @@ import ConsoleArea from "./curvelab/ConsoleArea";
 export default function CurveLab() {
   return (
     <Layout>
-      {/* Full height grid */}
-      <div className="h-[calc(100vh-80px)] w-full grid grid-rows-[2fr_1fr] grid-cols-[1.2fr_2fr] gap-3 p-3">
-        {/* Top Row */}
-        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-auto">
+      {/* Full fixed grid without scroll */}
+      <div
+        className="fixed inset-0 top-[72px] grid grid-rows-[2fr_1fr] grid-cols-[1.2fr_2fr] gap-[0.5rem] 
+                   bg-background text-text dark:bg-darkBackground dark:text-darkText p-[0.5rem]"
+      >
+        {/* Top row */}
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-hidden">
           <DataInput />
         </div>
-        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface">
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-hidden">
           <GraphArea />
         </div>
 
-        {/* Bottom Row */}
-        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-auto">
+        {/* Bottom row */}
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-hidden">
           <OutputSummary />
         </div>
-        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-auto">
+        <div className="border border-border dark:border-darkBorder rounded bg-surface dark:bg-darkSurface overflow-hidden">
           <ConsoleArea />
         </div>
       </div>
